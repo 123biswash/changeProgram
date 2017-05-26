c35f46e9bd0ac917cec2a2eeda0ccf6d9a6e0355
123biswash <biswash.adhikari@hotmail.com>
2017-05-26 10:16:14 -0400

    first commit

---
+++ ChangeProgram.java
@@ -0,0 +1,15 @@
+package ChangeProgram;
+public class ChangeProgram {
+ public static void main(String[] args) {
+ int x;
+ double y;
+ double z;
+ 
+ x = 5;
+ y = 9.5;
+ 
+ z = x * y;
+
+ System.out.println("The product is " + z);
+ }
+}
